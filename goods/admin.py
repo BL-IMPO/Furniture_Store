f928836696fb3374a0e5db29e9c4505da17f463c
BL-IMPO <nikitka,durne@gmail.com>
2025-06-17 20:16:38 +0600

    DB and fixtures added.

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Categories, Products
+
+# common way
+#admin.site.register(Categories)
+#admin.site.register(Products)
+
+# for autodilling
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
