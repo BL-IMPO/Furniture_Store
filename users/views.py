ebdbf25f9996af71572c3a4b2442cc75fcb85d04
BL-IMPO <nikitka,durne@gmail.com>
2025-06-25 20:35:19 +0600

    Login required decorator redirect to target page.

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Pomyślnie zalogowałeś się na swoje konto.")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
