df6a9ebd1517c2d546c9038aa0d1072f1be3f896
BL-IMPO <nikitka,durne@gmail.com>
2025-06-30 22:03:52 +0600

    Create order is ready.

--- orders/forms.py
+++ orders/forms.py
@@ -1,14 +1,18 @@
 from django import forms
 
 
-class CreateOrderFrom(forms.Form):
+class CreateOrderForm(forms.Form):
 
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
-    requires_delivery = forms.ChoiceField()
+    requires_delivery = forms.ChoiceField(choices=[
+            ("0", False),
+            ("1", True), ], )
     delivery_address = forms.CharField(required=False)
-    payment_on_get = forms.ChoiceField()
+    payment_on_get = forms.ChoiceField(choices=[
+            ("0", 'False'),
+            ("1", 'True'), ], )
 
     # The bad way to store data like this because we will be involved in frontend part
     #first_name = forms.CharField(
